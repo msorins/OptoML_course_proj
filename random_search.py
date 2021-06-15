c555165a1e67d35de934345a7e8f8312953a510b
Natasa <natasa.krco@epfl.ch>
2021-06-15 10:13:56 +0200

    change to loglikelihood, experiments

--- random_search.py
+++ random_search.py
@@ -3,13 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
+import random
 
-def estimate_gradient(w, x, y, m):
+def estimate_gradient(w, x, y, m, loss):
     
     u = np.random.normal(size = w.shape)
     
-    loss_w = mse(y, x, w)
-    loss_u = mse(y, x, w+m*u)
+    pred_w = sigmoid(x.dot(w))
+    pred_u = sigmoid(x.dot(w+m*u))
+    
+    loss_w = loss(y, pred_w)
+    loss_u = loss(y, pred_u)
     
     grad_est = (loss_u - loss_w) * u / m
     return grad_est, loss_w 
@@ -19,23 +23,32 @@ def mse(y, x, w):
     """
     Compute the mse loss.
     """
-    e = y - x.dot(w)
+    e = y - sigmoid(x.dot(w))
     mse = e.dot(e) / (2* len(e))
     return mse
 
+def neg_loglikelihood(y, x, w):
+    
+    pred = sigmoid(x.dot(w))
+    return (1-y).dot(np.log(1 - pred)) + y.dot(np.log(pred))
 
-def random_search(epochs, X_train, y_train, X_valid, y_valid, gamma, m):
+def random_search(epochs, X_train, y_train, X_valid, y_valid, gamma, m, loss):
     
     w = np.zeros(X_train.shape[1])
     losses = []
     valid_losses = []
     for e in range(epochs):
-        grad, loss_w = estimate_gradient(w, X_train, y_train, m)
-        valid_loss = mse(y_valid, X_valid, w)
+        #pred_train = sigmoid(X_train.dot(w))
+        pred_valid = sigmoid(X_valid.dot(w))
+        grad, loss_w = estimate_gradient(w, X_train, y_train, m, loss)
+        valid_loss = loss(y_valid, pred_valid)
         w = w - gamma * grad
         losses.append(loss_w)
         valid_losses.append(valid_loss)
-        if e%1000==0:
+        if e%10==0:
             print('loss_w = {}'.format(loss_w))
             
     return w, losses, valid_losses
+
+def sigmoid(t):
+    return 1.0 / (1 + np.exp(-t))
