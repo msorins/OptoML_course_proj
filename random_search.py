4e2506c3ba522523c7057ba26b55f41afbad1c97
Natasa <natasa.krco@epfl.ch>
2021-06-17 19:01:43 +0200

    add experiments, clean code

--- random_search.py
+++ random_search.py
@@ -14,39 +14,77 @@ def estimate_gradient(w, x, y, m, loss):
     
     loss_w = loss(y, pred_w)
     loss_u = loss(y, pred_u)
+    grad_est = (loss_u - loss_w) * u / m
+    return grad_est, loss_w 
+
+def estimate_gradient_regression(w, x, y, m, loss):
     
+    np.random.seed(0)
+    u = np.random.normal(size = w.shape)
+    
+    pred_w = x.dot(w)
+    pred_u = x.dot(w+m*u)
+    
+    loss_w = loss(y, pred_w)
+    loss_u = loss(y, pred_u)
     grad_est = (loss_u - loss_w) * u / m
     return grad_est, loss_w 
 
 
-def mse(y, x, w):
+def mse(y_true, y_pred):
     """
     Compute the mse loss.
     """
-    e = y - sigmoid(x.dot(w))
+    e = y_true - y_pred
     mse = e.dot(e) / (2* len(e))
     return mse
 
-def neg_loglikelihood(y, x, w):
+
+def random_search_regression(epochs, X_train, y_train, X_valid, y_valid, gamma, m, loss):
     
-    pred = sigmoid(x.dot(w))
-    return (1-y).dot(np.log(1 - pred)) + y.dot(np.log(pred))
+    w = np.zeros(X_train.shape[1])
+    losses = []
+    valid_losses = []
+    np.random.seed(111)
+    
+    for e in range(epochs):
+        
+        pred_valid = X_valid.dot(w)
+        valid_loss = loss(y_valid, pred_valid)
+        grad, loss_w = estimate_gradient_regression(w, X_train, y_train, m, loss)
+        
+        w = w - gamma * grad
+            
+        losses.append(loss_w)
+        valid_losses.append(valid_loss)
+        
+        if e%10==0:
+            print('loss_w = {}'.format(loss_w))
+            print('valid_loss = {}'.format(valid_loss))
+            
+    return w, losses, valid_losses
 
 def random_search(epochs, X_train, y_train, X_valid, y_valid, gamma, m, loss):
     
     w = np.zeros(X_train.shape[1])
     losses = []
     valid_losses = []
+    np.random.seed(0)
+    
     for e in range(epochs):
-        #pred_train = sigmoid(X_train.dot(w))
+        
         pred_valid = sigmoid(X_valid.dot(w))
-        grad, loss_w = estimate_gradient(w, X_train, y_train, m, loss)
         valid_loss = loss(y_valid, pred_valid)
+        
+        grad, loss_w = estimate_gradient(w, X_train, y_train, m, loss)
         w = w - gamma * grad
+            
         losses.append(loss_w)
         valid_losses.append(valid_loss)
+        
         if e%10==0:
             print('loss_w = {}'.format(loss_w))
+            print('valid_loss = {}'.format(valid_loss))
             
     return w, losses, valid_losses
 
